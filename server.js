dc8d5b8d566daa556d063969060973ff8626ed6f
joeaoregan <joe.oregan@gmail.com>
2025-07-01 23:55:33 +0100

    server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 3000;
+console.log(`Server is running on http://localhost:${port}`);
 app.listen(port);
